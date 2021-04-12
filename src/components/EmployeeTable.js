5de1d981530a3fdc75151405261aafaaa026270d
Haleigh Spurlock <haleighspurlock@hotmail.com>
2021-04-12 17:33:30 -0400

    sorting employee table working. staging for deploy

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -1,9 +1,16 @@
 import { useEffect, useState } from 'react';
 import API from '../utils/API';
 import { Table } from 'react-bootstrap'
+import { FaSortUp, FaSortDown } from 'react-icons/fa';
+
+const SortIcon = ({isAscending, size}) => {
+    let icon = isAscending ? <FaSortUp size={size}/> : <FaSortDown size={size} />
+    return <>{icon}</>
+}
 
 const EmployeeTable = ({filter}) => {
     const [initialEmployees, setInitialEmployees] = useState([])
+    const [sortName, setSortName] = useState('asc')
 
     useEffect(() => {
        getInitialEmployees()
@@ -17,19 +24,33 @@ const EmployeeTable = ({filter}) => {
     const fullName = (employee) => {
         return `${employee.name.first} ${employee.name.last}`
     }
+
+    const onSortNameClick = () => {
+        if(sortName === 'asc') {
+            setSortName('dsc')
+        } else {
+            setSortName('asc')
+        }
+    }
     
     let employees = initialEmployees.filter((emp)=>{
         const name = fullName(emp).toLowerCase()
         return name.startsWith(filter.toLowerCase())
     })
 
+    if(sortName === 'asc') {
+        employees = employees.sort((a, b)=>a.name.first > b.name.first ? 1 : -1)
+    } else {
+        employees = employees.sort((a, b)=>a.name.first > b.name.first ? -1 : 1)
+    }
+    
     return (
         <>
         <Table  striped bordered hover className='table'>
             <thead>
                 <tr>
                     <th>Picture</th>
-                    <th>Name</th>
+                    <th>Name <span onClick={onSortNameClick} style={{cursor: 'pointer'}}><SortIcon size={15} isAscending={sortName === 'asc'} /></span></th>
                     <th>Phone Number</th>
                     <th>Email</th>
                     <th>Employee ID</th>
